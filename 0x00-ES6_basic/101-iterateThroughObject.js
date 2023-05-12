de466fe67f7a409abc8245ec11b243452cf44b75
lyomu <gmnyomu@gmail.com>
2023-05-12 20:25:34 +0000

    101 fIX

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let i = 0;
-  let output = '';
-  for (const value in reportWithIterator) {
-    if (i !== reportWithIterator.length - 1) {
-      output += `${reportWithIterator[value]} | `;
-    } else {
-      output += reportWithIterator[value];
-    }
-    i += 1;
-  }
-
-  return output;
+  return reportWithIterator.join(' | ');
 }
